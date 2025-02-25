e5eb65993e79bdb2cc1d8c22bdd53e1eab9a2d73
Raxo96 <34631425+Raxo96@users.noreply.github.com>
2025-02-25 09:55:19 +0100

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -24,3 +24,24 @@
 # b) write GoldenAccount class that inherits Account,
 #    GoldenAccount can withdraw any amount of money without restrictions about balance.
 
+class Account:
+  pass
+
+class GoldenAccount:
+  pass
+
+client = Account('Bary', 100)
+print(client)
+
+client.deposit(50)
+assert client.balance == 150
+client.witdraw(100)
+assert client.balance == 50
+client.witdraw(50)
+assert client.balance == 0
+
+
+client2 = GoldenAccount('Bary', 100)
+print(client2)
+client2.witdraw(200)
+assert client.balance == -50
